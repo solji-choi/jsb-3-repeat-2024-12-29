135a51148314e74ea472d29c4e6fd693e2dad68e
solji <solji.choi89@gmail.com>
2024-12-30 01:40:33 +0900

    3-07

--- src/main/java/com/ll/sbb3/domain/user/user/repository/UserRepository.java
+++ src/main/java/com/ll/sbb3/domain/user/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.ll.sbb3.domain.user.user.repository;
 import com.ll.sbb3.domain.user.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
