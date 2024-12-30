a4d5e2bab27711a7118a373a6e2d1a2e61354047
solji <solji.choi89@gmail.com>
2024-12-31 01:44:07 +0900

    3-03

--- src/main/java/com/ll/sbb3/domain/question/question/repository/QuestionRepository.java
+++ src/main/java/com/ll/sbb3/domain/question/question/repository/QuestionRepository.java
@@ -1,6 +1,8 @@
 package com.ll.sbb3.domain.question.question.repository;
 
 import com.ll.sbb3.domain.question.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +13,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findByOrderByCreateDateDesc(Pageable pageable);
 }
