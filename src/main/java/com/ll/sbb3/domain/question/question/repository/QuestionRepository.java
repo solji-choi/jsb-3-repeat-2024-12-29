4ed3676be21d50c2864bffab8302c112d5202757
solji <solji.choi89@gmail.com>
2024-12-31 00:23:44 +0900

    3-13 1st fin

--- src/main/java/com/ll/sbb3/domain/question/question/repository/QuestionRepository.java
+++ src/main/java/com/ll/sbb3/domain/question/question/repository/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.ll.sbb3.domain.question.question.repository;
 import com.ll.sbb3.domain.question.question.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,4 +16,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     List<Question> findBySubjectLike(String subject);
 
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
--- src/main/java/com/ll/sbb3/domain/question/question/service/QuestionService.java
+++ src/main/java/com/ll/sbb3/domain/question/question/service/QuestionService.java
@@ -1,14 +1,17 @@
 package com.ll.sbb3.domain.question.question.service;
 
+import com.ll.sbb3.domain.question.answer.entity.Answer;
 import com.ll.sbb3.domain.question.question.entity.Question;
 import com.ll.sbb3.domain.question.question.repository.QuestionRepository;
 import com.ll.sbb3.domain.user.user.entity.SiteUser;
 import com.ll.sbb3.global.exceptions.DataNotFoundException;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,17 +24,38 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),
+                        cb.like(u1.get("username"), "%" + kw + "%"),
+                        cb.like(a.get("content"), "%" + kw + "%"),
+                        cb.like(u2.get("username"), "%" + kw + "%"));
+            }
+        };
+    }
+
     public List<Question> findAll() {
         return this.questionRepository.findAll();
     }
 
-    public Page<Question> findAll(int page) {
+    public Page<Question> findAll(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
 
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        Specification<Question> spec = search(kw);
 
-        return this.questionRepository.findAll(pageable);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public Question findById(Integer id) {
