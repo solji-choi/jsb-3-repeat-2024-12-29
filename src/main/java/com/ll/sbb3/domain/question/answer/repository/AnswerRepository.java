e3af32849506196628e18f9c8c833be0c03afda6
solji <solji.choi89@gmail.com>
2024-12-31 00:34:08 +0900

    Revert "setting"
    This reverts commit 1e943500bc13d3252b4df0c5f609c58cca59ad34.

--- src/main/java/com/ll/sbb3/domain/question/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.sbb3.domain.question.answer.repository;
-
-import com.ll.sbb3.domain.question.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
