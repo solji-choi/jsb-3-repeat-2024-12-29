6b6c657f97448d334a91cf86c9e5d055f58a69d7
solji <solji.choi89@gmail.com>
2024-12-31 23:10:12 +0900

    3-08

--- src/main/java/com/ll/sbb3/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbb3/domain/question/question/controller/QuestionController.java
@@ -3,21 +3,27 @@ package com.ll.sbb3.domain.question.question.controller;
 import com.ll.sbb3.domain.question.answer.form.AnswerForm;
 import com.ll.sbb3.domain.question.question.entity.Question;
 import com.ll.sbb3.domain.question.question.service.QuestionService;
+import com.ll.sbb3.domain.user.user.entity.SiteUser;
+import com.ll.sbb3.domain.user.user.service.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionService;
+    private final UserService userService;
 
     @GetMapping("/list")
     public String getItems(
@@ -54,21 +60,26 @@ public class QuestionController {
             String content
     ) {}
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String create(QuestionForm questionForm) {
         return "/domain/question/question/question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     public String create(
             @Valid QuestionForm questionForm,
-            BindingResult bindingResult
+            BindingResult bindingResult,
+            Principal principal
     ) {
         if(bindingResult.hasErrors()) {
             return "/domain/question/question/question_form";
         }
 
-        this.questionService.create(questionForm.subject, questionForm.content);
+        SiteUser author = this.userService.findByUsername(principal.getName());
+
+        this.questionService.create(questionForm.subject, questionForm.content, author);
 
         return "redirect:/question/list";
     }
