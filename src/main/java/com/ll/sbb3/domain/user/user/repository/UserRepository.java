77038a959148778a46657aaa7d07bdedb2a7c5ac
solji <solji.choi89@gmail.com>
2024-12-31 21:35:37 +0900

    3-06

---
+++ src/main/java/com/ll/sbb3/domain/user/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sbb3.domain.user.user.repository;
+
+import com.ll.sbb3.domain.user.user.entity.SiteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+}
---
+++ src/main/java/com/ll/sbb3/domain/user/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.ll.sbb3.domain.user.user.service;
+
+import com.ll.sbb3.domain.user.user.entity.SiteUser;
+import com.ll.sbb3.domain.user.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser signup(String username, String password, String email) {
+        SiteUser user = SiteUser
+                .builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .email(email)
+                .build();
+
+        return this.userRepository.save(user);
+    }
+}
